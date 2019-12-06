8557996ae7c4c3bd0bed6de12a0242019ffaa266
Badri <badri.py@outlook.com>
2019-12-06 21:36:04 +0530

    Adding the Shopping List Service to add ingredients

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RecipeService } from './recipe/recipe.service';
+import { ShoppingListService } from './shopping-list/shopping-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [RecipeService]
+  providers: [RecipeService, ShoppingListService]
 })
 export class AppComponent {
   loadedFeature = 'recipe';
