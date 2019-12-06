625abdc0164f88011a55ab68f2dfc1cf72089a56
Badri <badri.py@outlook.com>
2019-12-06 20:34:48 +0530

    Get Recipes from a Recipe Service

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,3 +1,18 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    
+    // making it private thus it won't be accessible outside this class
+    private recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+      ];
+
+    getRecipes() {
+        // returning the recipe as a new array
+        return this.recipes.slice();
+    }
+
+    addRecipes(recipe: Recipe) {
+        this.recipes.push(recipe);
+    }
 }
