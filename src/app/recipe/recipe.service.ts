37d244ab6c8bbb9628c777b136e772186cf02a6e
Badri <badri.py@outlook.com>
2019-12-06 21:54:37 +0530

    Adding the ingredient to the recipe detail component

--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -1,14 +1,29 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
-   
+
     // making it private thus it won't be accessible outside this class
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
-      ];
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Bread', 2)
+            ]),
+        new Recipe(
+            'A Test Recipe',
+            'This is simply a test',
+            'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('Bread', 2)
+            ])
+    ];
 
     getRecipes() {
         // returning the recipe as a new array
