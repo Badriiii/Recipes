eb9ff0016ce272becb5becabb817ec6afa7effc3
Badri <badri.py@outlook.com>
2019-12-12 22:53:01 +0530

    Navigation to the edit page

--- src/app/recipe/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe/recipe-edit/recipe-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-edit.component.css']
 })
 export class RecipeEditComponent implements OnInit {
-
-  constructor() { }
+  id: number;
+  editMode = false; // Initially assuming we are creating new recipe
+  constructor(private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    // subscribe allows us to call this whenever there is change in route params
+    // an observable method
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id']; // converting string to num using +
+        this.editMode = params['id'] != null;
+      }
+    );
   }
 
 }
