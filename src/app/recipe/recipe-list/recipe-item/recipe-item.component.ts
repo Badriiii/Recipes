f373de84e6ff8c496a1da94cc44e1c7ff627be5c
Badri <badri.py@outlook.com>
2019-12-12 22:37:16 +0530

    Passing dynamic params to the links

--- src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipe/recipe-list/recipe-item/recipe-item.component.ts
@@ -9,6 +9,7 @@ import { Recipe } from '../../recipe.model';
 export class RecipeItemComponent implements OnInit {
   // @Input - gets the data from parent component
   @Input() recipe: Recipe;
+  @Input() index: number;
 
   ngOnInit() {
   }
