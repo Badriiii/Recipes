f7098b9c264b394233cc6ab4c7004efcc7d1bd5a
Badri <badri.py@outlook.com>
2019-12-24 07:44:51 +0530

    Switching between login and signup mode

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,12 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-auth',
     templateUrl: 'auth.component.html'
 })
 
-export class AuthComponent implements OnInit {
-    constructor() { }
+export class AuthComponent {
+    isLoginMode = false;
 
-    ngOnInit() { }
+    onSwitchMode() {
+        this.isLoginMode = !this.isLoginMode;
+    }
 }
