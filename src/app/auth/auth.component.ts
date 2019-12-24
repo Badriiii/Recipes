3aa985c8566967e8033c530a59644e8d93308b2f
Badri <badri.py@outlook.com>
2019-12-24 08:23:12 +0530

    Sending the signup request to the firebase from UI

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
     selector: 'app-auth',
@@ -9,12 +10,30 @@ import { NgForm } from '@angular/forms';
 export class AuthComponent {
     isLoginMode = false;
 
+    constructor(private authService: AuthService) { }
+
     onSwitchMode() {
         this.isLoginMode = !this.isLoginMode;
     }
 
     onSubmit(form: NgForm) {
-        console.log(form.value);
+        if (!form.valid) {
+            return;
+        }
+
+        const email = form.value.email;
+        const password = form.value.password;
+        if (this.isLoginMode) {
+           // call login method in service
+        } else {
+            this.authService.signup(email, password)
+                .subscribe(resdata => {
+                    console.log(resdata);
+                }, error => {
+                    console.log(error);
+                });
+        }
+
         form.reset();
     }
 }
