c272dd36022d101d5fa3a4436bf355bd9834d920
Badri <badri.py@outlook.com>
2019-12-25 12:54:02 +0530

    refactor the auth component to use the observable to have common subscribe

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { AuthService } from './auth.service';
+import { AuthService, AuthResponseData } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-auth',
@@ -23,32 +24,26 @@ export class AuthComponent {
             return;
         }
 
+        let authObs: Observable<AuthResponseData>;
         const email = form.value.email;
         const password = form.value.password;
 
         this.isLoading = true;
         if (this.isLoginMode) {
-            this.authService.signin(email, password)
-                .subscribe(resdata => {
-                    console.log(resdata);
-                    this.isLoading = false;
-                }, errorMessage => {
-                    console.log(errorMessage);
-                    this.error = errorMessage;
-                    this.isLoading = false;
-                });
+            authObs = this.authService.signin(email, password);
         }
         else {
-            this.authService.signup(email, password)
-                .subscribe(resdata => {
-                    console.log(resdata);
-                    this.isLoading = false;
-                }, errorMessage => {
-                    console.log(errorMessage);
-                    this.error = errorMessage;
-                    this.isLoading = false;
-                });
+            authObs = this.authService.signup(email, password);
         }
+
+        authObs.subscribe(resdata => {
+            console.log(resdata);
+            this.isLoading = false;
+        }, errorMessage => {
+            console.log(errorMessage);
+            this.error = errorMessage;
+            this.isLoading = false;
+        });
 
         form.reset();
     }
