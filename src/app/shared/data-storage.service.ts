8a93364bd087de9e54f0e4f8a6f9ca30028bfea0
Badri <badri.py@outlook.com>
2019-12-26 07:50:38 +0530

    Adding the user token in query params to Outgoing Requests

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { RecipeService } from "../recipe/recipe.service";
 import { Recipe } from "../recipe/recipe.model";
-import { map, tap } from "rxjs/operators";
+import { map, tap, exhaustMap, take } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({ providedIn: 'root' })
 export class DataStorageService {
-    constructor(private http: HttpClient, private recipeService: RecipeService) {
-    }
+    constructor(private http: HttpClient,
+        private recipeService: RecipeService,
+        private authService: AuthService) { }
 
     storeRecipes() {
         const recipes = this.recipeService.getRecipes();
@@ -18,24 +20,30 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        // mentioning that the get type would be recipe array
-        // returning the observable so it could be subscribed from somewhere
-        return this.http.get<Recipe[]>('https://ng-recipe-book-84a51.firebaseio.com/recipes.json')
-            .pipe(
-                map(recipes => { // map allows us to transform the data by adding the pipe
-                    return recipes.map(recipe => {
-                        // checking if the recipe has ingredient
-                        // if not adding empty array instead of undefined using spread operator
-                        return {
-                            ...recipe,
-                            ingredients: recipe.ingredients ? recipe.ingredients : []
-                        }
-                    })
-                }),
-                // tap allows us to do some changes without altering the data 
-                tap(recipes => {
-                    this.recipeService.setRecipes(recipes)
+        return this.authService.user.pipe(
+            take(1), // subscribe to one user and then unsubscribe
+            exhaustMap(user => {  // exhaust map will wait for dirst subscribtion to complete and then will subscribes its own
+                return this.http.get<Recipe[]>(
+                    'https://ng-recipe-book-84a51.firebaseio.com/recipes.json',
+                    {
+                        params: new HttpParams().set('auth', user.token)
+                    }
+                )
+            }),
+            map(recipes => { // map allows us to transform the data by adding the pipe
+                return recipes.map(recipe => {
+                    // checking if the recipe has ingredient
+                    // if not adding empty array instead of undefined using spread operator
+                    return {
+                        ...recipe,
+                        ingredients: recipe.ingredients ? recipe.ingredients : []
+                    }
                 })
-            );
+            }),
+            // tap allows us to do some changes without altering the data 
+            tap(recipes => {
+                this.recipeService.setRecipes(recipes)
+            }))
+
     }
 }
