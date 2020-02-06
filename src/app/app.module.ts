b2359377c55e9f3840063b2e7a7f7f80978cac0a
Badri <badri.py@outlook.com>
2020-02-06 20:46:46 +0530

    Move the shopping list feature to seperate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component'; import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
-import { DropdownDirective } from './shared/dropdown.directive';
+import { HeaderComponent } from './header/header.component';import { DropdownDirective } from './shared/dropdown.directive';
 import { AppRoutingModule } from './app.routing.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 import { RecipeService } from './recipe/recipe.service';
@@ -16,13 +14,12 @@ import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { AlertComponent } from './shared/alert/alert.component';
 import { PlaceHolderDirective } from './shared/placeholder/placeholder.directive';
 import { RecipeModule } from './recipe/recipe.module';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -35,7 +32,8 @@ import { RecipeModule } from './recipe/recipe.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
-    RecipeModule
+    RecipeModule,
+    ShoppingListModule
   ],
   providers: [
     RecipeService,
--- src/app/recipe/recipe.module.ts
+++ src/app/recipe/recipe.module.ts
@@ -5,7 +5,6 @@ import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
-import { AppRoutingModule } from "../app.routing.module";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RecipeRoutingModule } from "./recipe-routing.module";
@@ -20,7 +19,6 @@ import { RecipeRoutingModule } from "./recipe-routing.module";
         RecipeEditComponent
     ],
     imports: [
-        AppRoutingModule,
         CommonModule,
         ReactiveFormsModule,
         RecipeRoutingModule
