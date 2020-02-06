97c20b9e9f518ed201f4ccfd15bced14b3586651
Badri <badri.py@outlook.com>
2020-02-06 21:15:01 +0530

    Move the auth related things to seperate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,32 +4,27 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component'; import { AppRoutingModule } from './app.routing.module';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
-import { RecipeService } from './recipe/recipe.service';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
+import { HttpClientModule } from '@angular/common/http'
 import { RecipeModule } from './recipe/recipe.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
 import { CoreModule } from './core.module';
+import { AuthModule } from './auth/auth.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    AuthComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipeModule,
     ShoppingListModule,
     SharedModule,
-    CoreModule
+    CoreModule,
+    AuthModule
   ],
   bootstrap: [AppComponent]
 })
